7156eb17f3e96b24de595af4a7eb43415d852982
Gregory Vogel <gmv23@cornell.edu>
2020-04-21 16:22:46 -0400

    Bioinformatics scripts untouched from when first written

---
+++ call_variants.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+# Sort and index bam files in parallel
+# Then divide alignments by chromosome
+# And run mpileup in parallel on each chromosome
+
+in_folder=$1
+out_folder=$2
+
+#set -x
+
+# Sort input bam files in parallel
+ls $in_folder/*.bam | parallel samtools sort {} -@ 4 -o {.}_sorted.bam
+
+# Make file with chromosome names
+grep ">" /workdir/gmv23/bsa/cpepo_genome/Cpepp_v4.1.chr.fa | sed 's/^.//' > $out_folder/chromosome_names.txt
+
+# Make file with names of input bam files
+ls $in_folder/*sorted.bam > $out_folder/sorted_bams.txt
+
+# Make file with names of bcf files for each chromosome
+cat $out_folder/chromosome_names.txt | sed 's/\./_/g' | sed 's/$/.bcf/' > $out_folder/bcf_chrom_files.txt
+
+# Index input bam files in parallel
+parallel samtools index -@ 4 {} :::: $out_folder/sorted_bams.txt
+
+# Run mpileup for each chromosome in parallel#
+parallel --link bcftools mpileup -A -q 20 -b $out_folder/sorted_bams.txt -a AD,INFO/AD \
+-f /workdir/gmv23/bsa/cpepo_genome/Cpepp_v4.1.chr.fa \
+-r {1} -o $out_folder/{2} --threads 3 -O b :::: $out_folder/chromosome_names.txt \
+:::: $out_folder/bcf_chrom_files.txt
+
+# Now concatenate all the separate bcf files together
+cd $out_folder
+bcftools concat -f bcf_chrom_files.txt -O b --threads 24 -o final_allchrom.bcf
+
+# Now call variants on pileup file 
+bcftools call -c -v -V indels --threads 24 -O v -o final_geno.vcf final_allchrom.bcf
