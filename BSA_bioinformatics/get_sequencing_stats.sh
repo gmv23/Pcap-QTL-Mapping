ac9a5ace13421594534132a89ccbd7ced0444ee9
Gregory Vogel <gmv23@cornell.edu>
2020-05-26 17:00:24 -0400

    get_sequencing_stats.sh should now report raw, filtered, and aligned base pairs

--- BSA_bioinformatics/get_sequencing_stats.sh
+++ BSA_bioinformatics/get_sequencing_stats.sh
@@ -4,15 +4,49 @@
 #raw reads
 #trimmed and filtered reads
 #reads aligning to genome
-#Then the median SNP depth will accompany as well
+#An R script will later append the median SNP read depth
+
+#Note that this script is not parallelized at all and will take a very long time to run
 
 path_raw_reads='/workdir/gmv23/bsa/data/round[12]'
+path_filtered_reads='/workdir/gmv23/squashQTL/bsa_bioinformatics_2020-04-30/trimming/trimmed_reads'
+path_bam_files='/workdir/gmv23/squashQTL/bsa_bioinformatics_2020-04-30/alignments/bam_files'
 
-path_filtered_reads=
+#Delete output file if it exists
+if [ -f sequencing_stats.txt ]; then
+	rm sequencing_stats.txt
+fi
 
+mkdir tmp
+
+#Loop through different pools
 for sample in D25 15_6015 1_S 1_T 2_S 2_T 2_R
 do
-	echo $sample
+	#Column for sample name
+	echo -n $sample >> sequencing_stats.txt
+	echo -n ',' >> sequencing_stats.txt
+
+	#Column for raw read stats
 	gunzip -c $path_raw_reads/*Cpep_11_[A-Z]0[0-9]_$sample* | cat | \
-	paste - - - - | cut -f 2 | tr -d '\n' | wc -c
+	paste - - - - | cut -f 2 | tr -d '\n' | wc -c | \
+	xargs echo -n >> sequencing_stats.txt	
+	echo -n ',' >> sequencing_stats.txt
+
+	#Column for trimmed read stats
+	gunzip -c $path_filtered_reads/*Cpep_11_[A-Z]0[0-9]_$sample* | cat | \
+	paste - - - - | cut -f 2 | tr -d '\n' | wc -c | \
+	xargs echo -n >> sequencing_stats.txt
+	echo -n ',' >> sequencing_stats.txt
+
+	#####Now get aligned bases
+	#Add up bases in each subpool
+	for file in $path_bam_files/[A-Z]0[0-9]_${sample}*sorted.bam
+	do
+		samtools view -b -q 20 $file | bedtools genomecov -ibam - -d | awk '{s+=$3}END{print s}' >> tmp/${sample}_aligned_bp.txt
+	done
+	#Add it all together and append to file
+	awk '{s+=$0}END{print s}' tmp/${sample}_aligned_bp.txt | xargs echo >> sequencing_stats.txt
+
 done
+
+#rm -r tmp
