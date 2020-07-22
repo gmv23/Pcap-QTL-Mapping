33133afbb5b89629b17194df0c782094dcf5187f
Gregory Vogel <gmv23@cornell.edu>
2020-07-22 09:22:34 -0400

    Separated boxplot into 5 different boxplots

--- LinkageMapping/PlotPhenotypes.R
+++ LinkageMapping/PlotPhenotypes.R
@@ -13,14 +13,14 @@ colnames(geno)[1] <- "Sample"
 pdf("plots/phenotypic_distributions.pdf", width=7, height = 6)
 
 #Get plot layout
-m <- rbind(c(1,1,2,3,3,4),
-           c(5))
+m <- rbind(rep(c(1,1,2,3,3,4),each=5),
+           rep(c(5,6,7,8,9),each=6))
 layout(m)
 
 #Set par settings
 old.par <- par(no.readonly = T)
-par(oma=c(1,1,1,1), 
-    mar=c(5,4,2,1)
+par(oma=c(3,3,1,1), 
+    mar=c(6,2,3,3)
 )
 
 #Get data subsets for histogram
@@ -48,8 +48,7 @@ abline(v=blues$raudpc[which.pcny21], lwd=2, lty=3)
 
 #Plot 1 legend
 par(xpd=NA)
-text(x=-35,y=-20,labels="C", font=1,cex=2)
-text(x=-35,y=45,labels="A", font=1,cex=2)
+text(x=-35,y=45,labels="A", font=1,cex=1.5)
 plot(0, type='n', xaxt='n', yaxt='n',
      xlab='', ylab='', bty='n',
      xlim=c(0,10),ylim=c(0,10))
@@ -58,7 +57,7 @@ legend(-17,10, bty='n',
        legend = c("Dunja", 
                   expression(atop("Pc-NY21",
                                   paste("x Dunja ", "F"["1"]))), 
-                                  "Pc-NY21"),
+                  "Pc-NY21"),
        y.intersp=2)
 
 par(xpd=F)
@@ -87,7 +86,7 @@ points(x=rep(median(blues$raudpc[which.sel]),25), y = seq(0,40,length.out = 25),
        cex=0.7,bg=colors()[630], col=colors()[190], pch=22)
 #Plot 2 legend
 par(xpd=NA)
-text(x=-35,y=45,labels="B", font=1,cex=2)
+text(x=-35,y=45,labels="B", font=1,cex=1.5)
 
 plot(0, type='n', xaxt='n', yaxt='n',
      xlab='', ylab='', bty='n',
@@ -120,65 +119,71 @@ legend(-16,7, bty='n',
                                   paste(" selected ", "F"["2:3"])))),
        y.intersp=2.5)
 par(xpd=F)
-#Plot 3: phenotypes depending on QTL genotype
+
+#Plot 3-7: phenotypes depending on QTL genotype
+
+#New par
+par(mar=c(5,1.75,4,1.75), bty='n', xpd=NA)
 
 #Get rid of checks
 blues.f2 <- blues[blues$Sample %in% geno$Sample,]
 geno <- geno[match(blues.f2$Sample, geno$Sample),]
 
-#Get list with phenotypes for each QTL allele
-blues.cond <- list()
 for(i in 1:5){
-  for(j in 0:2){
-    blues.ij <- blues.f2$raudpc[which(geno[,(i+1)] == j)]
-    blues.name <- paste(i,j,sep="_")
-    blues.cond[[blues.name]] <- blues.ij
+  qtl.geno <- geno[,(i+1)]
+  #Get boxplot coordinates
+  boxplot.data <- boxplot(blues.f2$raudpc~qtl.geno, plot=F)
+  
+  if(i == 1){
+    ylab <- "rAUDPC"
+    xlab <- ""
+  }else if(i == 3){
+    xlab <- "Marker genotype"
+    ylab <- ""
+  }else{
+    ylab <- ""
+    xlab <- ""
   }
-}
-
-#Conduct Tukeys HSD for each QTL comparing alleles
-letter_assignments <- c()
-for(i in 1:5){
-  data = data.frame("raudpc" = blues.f2$raudpc,
-                    "geno" = as.factor(geno[,(i+1)]))
-  data.lm <- lm(raudpc~geno,data=data)
+  #Draw boxplot
+  boxplot(blues.f2$raudpc~qtl.geno,
+          names=c("AA","AB","BB"),
+          col=brewer.pal(3,"Dark2"),
+          ylab = ylab, xlab=xlab,
+          cex.lab=1.1,
+          ylim=c(min(blues.f2$raudpc),100),
+          yaxt = 's')
+  
+  #Add marker name
+  text(2,110,
+       label=gsub("X","",colnames(geno)[i+1]),
+       font=4)
+  
+  #Add letter
+  text(-0.6,120,LETTERS[(i+2)], cex=1.5)
+  
+  #Tukeys
+  data.lm <- lm(blues.f2$raudpc~qtl.geno)
   data.aov <- aov(data.lm)
-  data.hsd <- HSD.test(data.aov, "geno")
+  data.hsd <- HSD.test(data.aov, "qtl.geno")
   data.groups <- data.hsd$groups
   data.groups <- data.groups[order(row.names(data.groups)),]
-  letter_assignments <- c(letter_assignments, as.character(data.groups$groups))
+  
+  #Add Tukey letters
+  text(x=1:3,
+       y=boxplot.data$stats[5,]+5,
+       labels=as.character(data.groups$groups))
+  
+  if(i==3){
+    legend(-9,-18, bty='n', ncol=3,
+           fill = brewer.pal(3,"Dark2"),
+           legend = c("AA = Homozygous Dunja",
+                      "AB = Heterozygous",
+                      "BB = Homozygous Pc-NY21"),
+           cex=1.1,
+           x.intersp=0.6,text.width=6.5)
+  }
+  
 }
-
-#Where will the boxplots be
-at <- c(1,2,3,6,7,8,11,12,13,16,17,18,21,22,23)
-
-#Get boxplot coordinates
-boxplot.data <- boxplot(blues.cond, plot=F)
-
-#Draw boxplot
-boxplot(blues.cond,at=at,xaxt='n', 
-        col=rep(brewer.pal(3,"Dark2"),5),
-        ylab = "rAUDPC",
-        cex.lab=1.1,
-        ylim=c(min(blues.f2$raudpc),100))
-
-#Add Tukey letters
-text(x=at,y=boxplot.data$stats[5,]+5,labels=letter_assignments)
-
-#Add QTL names to x axis
-axis(side=1,at=c(2,7,12,17,22),labels=
-       gsub("X","",colnames(geno)[-1]),
-     cex.axis=1.2)
-
-#Plot 3 legend
-par(xpd=NA)
-legend(1,-1, bty='n', ncol=3,
-       fill = brewer.pal(3,"Dark2"),
-       legend = c("Homozygous Dunja",
-                  "Heterozygous",
-                  "Homozygous Pc-NY21"),
-       cex=1.1,
-       x.intersp=0.6,text.width=6.5)
 
 par(old.par)
 dev.off()
